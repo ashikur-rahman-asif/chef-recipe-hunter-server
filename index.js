5e87a2df87ff1ad78ba11e34707d280053b7f578
asif247 <86229363+asif247@users.noreply.github.com>
2023-05-03 06:41:13 +0600

    cors install for avoiding fetching data problem

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const app = express()
+const cors = require("cors");
 const port = process.env.PORT || 5000;
-
+const chefs = require('./data/chefsRecipe.json')
+app.use(cors());
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('The chefs recipe server running,,,')
 })
-
+app.get("/chefs", (req, res) => {
+    res.send(chefs);
+  });
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`The chefs recipe api server running,,,' ${port}`);
 })
